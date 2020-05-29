9cafca0d09215ae4e4b6f22d63eed81ead1856ec
Thiago Guimaraes <thiagogsr@gmail.com>
2020-05-28 23:40:43 -0300

    Force SSL on express

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const path = require("path");
 const app = express();
 const port = process.env.PORT;
 
+app.use(function (req, res, next) {
+  if (req.get("X-Forwarded-Proto") !== "https") {
+    res.redirect("https://" + req.get("Host") + req.url);
+  } else next();
+});
+
 app.use(express.static(path.join(__dirname, "build")));
 
 app.get("/*", function (req, res) {
