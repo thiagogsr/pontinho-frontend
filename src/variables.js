b7a6699b497092e1faa8405978692e075d78cc75
Thiago Guimaraes <thiagogsr@gmail.com>
2020-05-28 23:28:43 -0300

    Fix environment variable

--- src/variables.js
+++ src/variables.js
@@ -1,5 +1,5 @@
 export const BACKEND_ENDPOINT =
-  process.env.BACKEND_ENDPOINT || "http://localhost:4000";
+  process.env.REACT_APP_BACKEND_ENDPOINT || "http://localhost:4000";
 
 export const SOCKET_ENDPOINT =
-  process.env.SOCKET_ENDPOINT || "ws://localhost:4000/socket";
+  process.env.REACT_APP_SOCKET_ENDPOINT || "ws://localhost:4000/socket";
